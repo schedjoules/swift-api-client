22a1104298383d4632b029037e1a6a2e81e896c3
Balázs Vincze <bales1498@gmail.com>
2018-04-23 09:56:24 +0200

    Added queries to fetch the list of supported languages and countries, resolves #16. (#45)

---
+++ ApiClient/Source/Localization/Models/Protocols/Language.swift
@@ -0,0 +1,14 @@
+//
+//  Language.swift
+//  ApiClient
+//
+//  Created by Balazs Vincze on 2018. 04. 18..
+//  Copyright © 2018. SchedJoules. All rights reserved.
+//
+
+import Foundation
+
+public protocol Language {
+    var name: String { get }
+    var code: String { get }
+}
