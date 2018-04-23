6b5ae349886c42004e2e23bf3290e2eeb7c62a4e
Balazs Vincze <bales1498@gmail.com>
2018-04-23 11:25:37 +0200

    Removed `Content-Type` header from queries, fixes #47.

--- ApiClient/Source/Pages/Queries/SinglePageQuery.swift
+++ ApiClient/Source/Pages/Queries/SinglePageQuery.swift
@@ -32,7 +32,7 @@ public final class SinglePageQuery: Query {
     public let url: URL
     public let method: HTTPMethod = .get
     public let parameters: Parameters = [:]
-    public let headers: HTTPHeaders = ["Accept" : "application/json", "Content-Type" : "application/json"]
+    public let headers: HTTPHeaders = ["Accept" : "application/json"]
     
     private init(pageID: String, queryItems: [URLQueryItem]) {
         // Initialize url components from a string
